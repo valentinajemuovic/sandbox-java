19d2f980b00294078b0355057d103669e72e8fbd
Valentina Cupać <valentina.cupac@optivem.com>
2023-06-30 13:35:49 +0200

    #3 Height Classifier - Prepare for new project

--- height-classifier-2/src/main/java/com/optivem/sandbox/core/HeightClassifier.java
+++ height-classifier-2/src/main/java/com/optivem/sandbox/core/HeightClassifier.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 public class HeightClassifier {
+    private static BigDecimal CENTIMETERS_PER_INCH = BigDecimal.valueOf(2.54);
     private static final BigDecimal TALL_THRESHOLD = BigDecimal.valueOf(180);
 
     private HeightGateway heightGateway;
@@ -14,7 +15,7 @@ public class HeightClassifier {
 
     public boolean isTall(String ssn) {
         var heightInches = heightGateway.getHeightInches(ssn);
-        var heightCentimeters = heightInches.multiply(Constants.CENTIMETERS_PER_INCH, MathContext.DECIMAL64);
+        var heightCentimeters = heightInches.multiply(CENTIMETERS_PER_INCH, MathContext.DECIMAL64);
         return heightCentimeters.compareTo(TALL_THRESHOLD) > 0;
     }
 }
